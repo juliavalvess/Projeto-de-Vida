2af9356441160a180a1b6d16c7d97578b73458ce
juliavalvess <julia.pereira.alves@escola.pr.gov.br>
2026-04-22 16:05:01 -0300

    22/04

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const botoes = document.querySelectorAll(".botao");
  console.log(botoes);
 
  for (let i=0; i<=botoes.length;i++){
-    console.log(botoes[i]);
+
     botoes[i].onclick=function(){
         botoes[i].classList.add("ativo");
     }
