5f2f4ea20a80fac458076a1e6e64768d7dc566cd
juliavalvess <julia.pereira.alves@escola.pr.gov.br>
2026-05-13 15:45:48 -0300

    13/05

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const botoes = documento . querySelectorAll ( ".botao" );
+const botoes = document . querySelectorAll ( ".botao" );
 
 para ( let i = 0 ; i < botoes.length ; i ++){
 botoes [ i ]. onclick = function (){
 
 para ( seja j = 0 ; j < botoes.length ; j ++){
-botoes [ j ]. lista de classes . remover ( "ativo" );
+botoes [ j ].classList . remover ( "ativo" );
 }
-botoes [ eu ]. lista de classes . add ( "ativo" );
+botoes [ eu ].classList. add ( "ativo" );
 }
 }
